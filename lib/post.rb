59abbc3b8ab6fc43e1d47d91197a25a795c5f2a4
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-07 07:45:55 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,5 +1,7 @@
+
+#A post should be able to tell you the name of its author:
 class Post
-attr_accessor :author, :title
+attr_accessor :author, :title #Posts should have a title and belong to an author.
   def initialize
   @title = "title"
   end
